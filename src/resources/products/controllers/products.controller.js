58ea7247c050714277a8a03562e1f6a2b9c2744c
MatiasRomeo <m.romeov@hotmail.com>
2023-07-16 23:28:50 -0400

    final

--- src/resources/products/controllers/products.controller.js
+++ src/resources/products/controllers/products.controller.js
@@ -6,5 +6,30 @@ const body = req.body
 body["id"] = generateUUID()
 products.push(body)
 const productCreated = products.finds(product => product.id == id)
-return res.json(productCreated)
+return res.status(201).json(productCreated)
+}
+
+export const getProducts = (req, res) => {
+    return res.status(200).json ( products)
+}
+export const updateProductbyId = (req, res) => {
+    const productIndex = products.findIndex (products => product.id == id )
+    products = [productIndex ] = body
+    return res.status(200).json ( products[productIndex])
+}
+
+export const deleteById = (req, res) => {
+    const id = req.params.id
+}
+
+export const getProductsById = (req, res) => {
+    const id = req.params.id
+    const body = req.body
+    const productFound = products.find( product => product.id == id)
+    if (!productFound)
+    return res.status (404.).json( {status: "error", msg: "producto no encontrado"})
+    const productIndex = products.findIndex= (product => product.id ==id ) 
+    const productRemoved = products.splice(productIndex, 1) 
+    return res.status(200).json ( productsRemoved[0])
+    
 }
--- src/resources/products/routes/products.routes.js
+++ src/resources/products/routes/products.routes.js
@@ -1,14 +1,14 @@
 import {Router} from 'express'
-import { createProduct } from '../controllers/products.controller.js'
+import { createProduct, deleteById, getProducts, getProductsById, updateProductbyId } from '../controllers/products.controller.js'
 
 const productsRouter = Router ()
 const baseURI = '/products'
 
 productsRouter.post(baseURI, createProduct)
-productsRouter.get(baseURI)
-productsRouter.post(`${baseURI}/:id`)
-productsRouter.put(`${baseURI}/:id`)
+productsRouter.get(baseURI, getProducts)
+productsRouter.post(`${baseURI}/:id`, getProductsById)
+productsRouter.put(`${baseURI}/:id`, updateProductbyId)
 productsRouter.patch(`${baseURI}/:id`)
-productsRouter.delete(`${baseURI}/:id`)
+productsRouter.delete(`${baseURI}/:id`, deleteById)
 
 export default productsRouter
