c1d2889276d558ee5ff351fa9f36325a16e89a77
MatiasRomeo <m.romeov@hotmail.com>
2023-07-16 22:46:26 -0400

    PUERTO

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
 import environment from "./config/environment.js";
-import express, {json} from 'express'
+import express, { json } from 'express';
 import productsRouter from "./resources/products/routes/products.routes.js";
 
-const {PORT} = environment
-const app = express ()
-app.use(json())
+const { PORT } = environment;
+const app = express();
+app.use(json());
 
-app.get('/', function(req, res){
-return res.send( {"msg": "Hola mundo"})
-})
+app.get('/', function(req, res) {
+  return res.send({ "msg": "Hola mundo" });
+});
 
-//Rutas de los recursos
-app.use(productsRouter)
+// Rutas de los recursos
+app.use(productsRouter);
 
-app.listen(PORT, () => { console.log(`APLICACION INICIARA EN EL PUERTO ${PORT}`)})
+// Start the server
+const server = app.listen(PORT, () => {
+  console.log(`Aplicación iniciará en el puerto ${PORT}`);
+});
+
+server.on('error', (error) => {
+  if (error.code === 'EADDRINUSE') {
+    console.error(`El puerto ${PORT} está siendo utilizado por otro proceso.`);
+  } else {
+    console.error('Ocurrió un error al iniciar el servidor:', error);
+  }
+});
--- src/config/environment.js
+++ src/config/environment.js
@@ -3,7 +3,6 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export default {
-  PORT: process.env.PORT || 3500,
-  DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
-  TOKEN_SECRET: process.env.TOKEN_SECRET
+  PORT: process.env.PORT || 4000,
+
 }
