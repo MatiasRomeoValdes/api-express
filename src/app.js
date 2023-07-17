e99fff2094dae877f832b4819b9ec054ebade88f
MatiasRomeo <m.romeov@hotmail.com>
2023-07-16 22:04:27 -0400

    apirutas

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+import environment from "./config/environment.js";
 import express, {json} from 'express'
+
+const {PORT} = environment
 const app = express ()
 app.use(json())
 
@@ -7,4 +10,4 @@ return res.send( {"msg": "Hola mundo"})
 })
 
 
-app.listen(3000)
+app.listen(PORT, () => { console.log(`APLICACION INICIARA EN EL PUERTO ${PORT}`)})
---
+++ src/resources/products/routes/products.routes.js
@@ -0,0 +1,5 @@
+import {Router} from 'express'
+
+const productsRouter = Router ()
+const baseURI = '/products'
+
